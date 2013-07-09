eaf7e5970d6e55d51f9c6d6ac0cbfbc003a534ca
termick <christos_karapiperis@yahoo.gr>
2013-07-09 08:39:55 +0300

    updates index.html, web.js

--- web1.js
+++ web1.js
@@ -1,11 +1,9 @@
 var express = require('express');
 
 var app = express.createServer(express.logger());
-var fs=require('fs');
-app.get('/', function(request, response) {
-var buf = new Buffer(fs.readFileSync('index.html'),'utf-8');
 
-  response.send(buf.toString());
+app.get('/', function(request, response) {
+  response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
